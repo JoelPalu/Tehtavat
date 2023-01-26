3aeb7aa5e5c5f2dda8d2b0d8a3e45ab440560a91
JoelPalu <122279670+JoelPalu@users.noreply.github.com>
2023-01-26 15:38:34 +0200

    Module 6 tehtävä 6. Matikka korjaus

--- Module 6/teht6.py
+++ Module 6/teht6.py
@@ -1,8 +1,9 @@
 def pizza_worth(size,prise):
     import math
 
-    m2 = size * math.pi
-    worth = m2/prise
+
+    m2 = size/1000 * math.pi
+    worth = prise/m2
     return worth
 
 
@@ -11,8 +12,8 @@ while True:
     pp1 = float(input("Anna pizza 1 hinta €: "))
     ps2 = float(input("Anna pizza 2 halkasija cm: "))
     pp2 = float(input("Anna pizza 2 hinta €: "))
-    if pizza_worth(ps1,pp1) < pizza_worth(ps2,pp2):
+    if pizza_worth(ps1,pp1) > pizza_worth(ps2,pp2):
         print(f"Pizza 2 {pizza_worth(ps2,pp2):1.2f} €/m2 on enemmän worth ku pizza 1 {pizza_worth(ps1,pp1):1.2f} €/m2")
-    elif pizza_worth(ps1,pp1) > pizza_worth(ps2,pp2):
+    elif pizza_worth(ps1,pp1) < pizza_worth(ps2,pp2):
         print(f"Pizza 1 {pizza_worth(ps1, pp1):1.2f} €/m2 on enemmän worth ku pizza 2 {pizza_worth(ps2, pp2):1.2f} €/m2")
     else: print(f"Pizzat ovat yhtä suuret hinnoiltaan. {pizza_worth(ps1, pp1):1.2f} €/m2 ")
