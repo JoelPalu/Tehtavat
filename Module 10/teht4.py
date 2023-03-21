99b72c9fdbd4502ece9454bb6531f90aa73eef5a
JoelPalu <122279670+JoelPalu@users.noreply.github.com>
2023-03-21 09:53:38 +0200

    pikku hienouksia, ei varsianista koodi muuttoa.

--- Module 10/teht4.py
+++ Module 10/teht4.py
@@ -20,8 +20,6 @@ class Auto:
         self.traveled += self.speed * hour
 
 
-    pass
-
 class Race:
 
     def __init__(self, name, leinght, cars):
@@ -31,6 +29,7 @@ class Race:
         for n in range(1, cars+1):
             car = Auto("ABC-" + str(n), random.randint(100, 200))
             self.cars.append(car)
+
     def hour_update(self):
         for car in self.cars:
             car.acceleration(random.randint(-10, 15))
@@ -45,18 +44,19 @@ class Race:
         for car in self.cars:
             if car.traveled > self.leinght:
                 return True
-        else: return False
+        else:
+            return False
 
 
 race = Race("Suuri romuralli", 8000, 10)
 while True:
-    if race.race_over() == True:
+    if race.race_over() is True:
         print(f"|{'!RACE OVER!':^43}|")
         race.status()
         break
     race.status()
-    sleep(0.5)
+    sleep(1)
     for n in range(10):
         race.hour_update()
-        if race.race_over() == True:
+        if race.race_over() is True:
             break
